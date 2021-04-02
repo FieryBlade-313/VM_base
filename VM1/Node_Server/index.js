7ef16acb91e4531b2ce021c7f9ffe72ed65798ff
FieryBlade-313 <mohdfauz.s18@iiits.in>
2021-04-02 23:34:39 +0530

    Added the code for both VMs

---
+++ VM1/Node_Server/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+const routes = require('./routes')
+var cors = require('cors')
+
+var app = express()
+app.use(cors())
+
+routes(app)
+
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", "localhost", port)
+})
---
+++ VM1/Node_Server/populator.js
@@ -0,0 +1,7 @@
+db = require('./db.js')
+
+db.saveData("S20180010160", "Shaikh Mohd Fauz");
+db.saveData("S20180010002", "Abhinav Anand");
+db.saveData("S20180010153", "Samandeep Singh");
+db.saveData("S20180010154", "Sathyanarayan R");
+db.saveData("S20180010069", "Jatin Mishra");
