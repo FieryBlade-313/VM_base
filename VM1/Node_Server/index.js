fb0cc775969b4d3348f94bba90cc47f51d4ecf69
FieryBlade-313 <mohdfauz.s18@iiits.in>
2021-04-03 13:27:04 +0530

    Added the IP address to the static IP

--- VM1/Node_Server/index.js
+++ VM1/Node_Server/index.js
@@ -11,5 +11,5 @@ var server = app.listen(3000, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", "13.90.100.116", port)
+    console.log("Example app listening at http://%s:%s", "137.117.35.253", port)
 })
